2f73f3af386fb92e2630f9a60c33e0928e1589f6
Sultepronit <sultepronit@gmail.com>
2026-03-30 21:08:28 +0300

    upgraded to PWA

---
+++ vite.config.ts
@@ -0,0 +1,73 @@
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            // devOptions: { enabled: true },
+            includeAssets: ["favicon.ico", "/fonts/*.ttf", "/fonts/*.otf"],
+            manifest: {
+                name: "Learn Japanese",
+                short_name: "LearnJap",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "/pwa-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable"
+                    },
+                    {
+                        src: "/pwa-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ],
+                start_url: "/",
+                display: "standalone",
+                background_color: "#bc002d",
+                theme_color: "#bc002d",
+                description: "My app for learning Japanese vocabulary"
+            },
+            workbox: {
+                runtimeCaching: [
+                    {
+                        // fonts
+                        urlPattern: ({ request }) => request.destination === "font",
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "fonts"
+                            // cacheableResponse: {
+                            //   statuses: [0, 200]
+                        }
+                    },
+                    {
+                        // audio
+                        urlPattern: ({ request }) => request.destination === "audio",
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "audio",
+                            expiration: {
+                                maxAgeSeconds: 60 * 60 * 24 * 365 * 2
+                            }
+                        }
+                    }
+                ]
+            }
+        })
+    ]
+})
